d77767e42ce5655b97afeae7357ac05cae07db0f
Lavaugh Brown <lavaugh@usf.edu>
2026-05-01 21:31:58 -0400

    fix: hardcode base path to / for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Invicta-Attorney/' : '/',
+  base: '/',
   server: {
     host: '127.0.0.1',
     port: 3000,
